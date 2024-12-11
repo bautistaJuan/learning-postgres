eab27853a4cf4dc0880f051dbcd8d063b0f164fd
Juan Bautista Vega <jv604824@gmail.com>
2024-12-11 15:24:09 -0300

    Generamos un token para el usuario con JWT para poder verificar correctamente los datos. Creacion del endpoint 'auth/token'.

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ import * as crypto from "crypto";
 import { sequelize } from "./db/sequelize";
 import { User } from "./db/users";
 import { Auth } from "./db/auth";
-
+import * as jwt from "jsonwebtoken";
 // sequelize.sync({ force: true }).then(res => {
 //   console.log(res);
 // });
@@ -13,6 +13,8 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 const port = process.env.PORT || 3000;
+
+const SECRET = "asd32asd32a111asddd33awd";
 
 const getSHA256ofString = function (text: string) {
   return crypto.createHash("sha256").update(text).digest("hex");
@@ -43,6 +45,27 @@ app.post("/auth", async (req, res) => {
   });
   console.log({ auth, authCreated });
   res.json(user);
+});
+
+// Signin
+app.post("/auth/token", async (req, res) => {
+  const { email, password } = req.body;
+  const passwordHasheado = getSHA256ofString(password);
+
+  const auth = await Auth.findOne({
+    where: {
+      email,
+      password: passwordHasheado,
+    },
+  });
+
+  const token = jwt.sign({ id: auth.getDataValue("user_id") }, SECRET);
+
+  if (auth) {
+    res.json({ token });
+  } else {
+    res.status(401).json({ error: "No autorizado" });
+  }
 });
 
 app.listen(port, () => {
